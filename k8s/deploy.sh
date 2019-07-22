6c6faa0d25ce1eaa18df391ef2f1a4771639ba6b
Simon Frid <simon.frid@gmail.com>
2019-07-22 12:37:28 -0700

    CI-84 tweek deployment sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -12,29 +12,32 @@ BASE="$CWD/base"
 # service specific vars
 WORKSPACE_NAME=carrera
 SERVICE_NAME=api
-GCP_PROJECT=devopspatterns
+GCP_PROJECT_ID=devopspatterns
+GCP_ZONE=us-central1-a
+CLUSTER_NAME=hosting-applications
 
 # derivative vars
-DOCKER_REPOSITORY="gcr.io/${GCP_PROJECT_ID}/"
+DOCKER_REPOSITORY="gcr.io/${GCP_PROJECT_ID}"
 TAG="${GIT_COMMIT_SHA}.${DEPLOY_SCRIPT_TIMESTAMP}"
 DOCKER_SERVICE_TAG="${SERVICE_NAME}:${TAG}"
 DEPLOYMENT_ANNOTATION="Git commit ${GIT_COMMIT_SHA_SHORT} deployed at ${DEPLOY_SCRIPT_TIMESTAMP_PRETTY} - ${DOCKER_IMAGE_TAG}"
 
 # get cluster creds from gcloud
-gcloud container clusters get-credentials hosting-applications --zone us-central1-a --project $GCP_PROJECT
+gcloud container clusters get-credentials $CLUSTER_NAME --zone $GCP_ZONE --project $GCP_PROJECT_ID
 
-# docker tag
+# docker build and tag
 DOCKER_IMAGE_TAG="${DOCKER_REPOSITORY}/${DOCKER_SERVICE_TAG}"
-docker tag node:9.8-alpine $DOCKER_IMAGE_TAG
+#docker tag node:9.8-alpine $DOCKER_IMAGE_TAG
+docker build -f ../Dockerfile -t $DOCKER_IMAGE_TAG .
 
 # push
-gcloud docker -- push $DOCKER_IMAGE_TAG
+echo "pushing $DOCKER_IMAGE_TAG"
+docker push $DOCKER_IMAGE_TAG
 
 # compile manifests
 #kustomize edit set image carrera-api:$(git log -n 1 --pretty=format:"%H")
-kustomize edit set image carrera-api=DOCKER_IMAGE_TAG
+cd base && kustomize edit set image carrera-api=$DOCKER_IMAGE_TAG && cd ..
 
+# build and apply manifests
 kustomize build base | kubectl apply -f -
 
-
-# apply manifests
